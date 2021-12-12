b7b84ccc3b943009462a6cee6ad61eaaf170482d
Timur Vorobyov <buskettim0902@gmail.com>
2021-12-12 08:55:48 +0500

    chore: sets global styles

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import { AppProps } from "next/app";
+import "../styles/global.scss";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
