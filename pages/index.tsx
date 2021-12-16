79d684e74c6cf397e4a40be0664e7dac03112dd6
Timur Vorobyov <buskettim0902@gmail.com>
2021-12-16 11:13:52 +0500

    feat: creates home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,72 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { IСurrency } from "../types/currency";
+import index from "./index.module.scss";
+
 const Index = () => {
-  return <></>;
+  const { currencies } = useTypedSelector((state) => state.currency);
+  const { fetchCurrencies } = useActions();
+  const router = useRouter();
+
+  useEffect(() => {
+    fetchCurrencies();
+    const id = setInterval(() => fetchCurrencies(), 10000);
+    return () => clearInterval(id);
+  }, []);
+
+  const handleClick = (id: number) => {
+    router.push(`/currency/${id}`);
+  };
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell className={index.header}>Currency name</TableCell>
+            <TableCell className={index.header}>Currency price</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {currencies.map((currency: IСurrency) => (
+            <TableRow
+              className={index.row}
+              key={currency.id}
+              onClick={() => {
+                handleClick(currency.id);
+              }}
+            >
+              <TableCell component="th" scope="row">
+                {currency.name}
+              </TableCell>
+              <TableCell>
+                {"$" +
+                  Math.round(
+                    (currency.quote.USD.price + Number.EPSILON) * 10000
+                  ) /
+                    10000}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default Index;
+
+function useHistory() {
+  throw new Error("Function not implemented.");
+}
